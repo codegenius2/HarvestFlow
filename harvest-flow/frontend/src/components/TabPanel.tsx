ff2e6902ee1882aab812c20e952b9b0b05c3383a
ryo0803 <rn.383.b2fz@gmail.com>
2024-05-01 23:08:47 +0200

    Add tabs panel to Project page

---
+++ harvest-flow/frontend/src/components/TabPanel.tsx
@@ -0,0 +1,30 @@
+import {Box, Typography} from "@mui/material";
+import React from "react";
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+export default TabPanel;
--- harvest-flow/frontend/src/pages/Project.tsx
+++ harvest-flow/frontend/src/pages/Project.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import Layout from "@src/layouts/Layout";
-import Header from "@src/components/Header";
+import {Box, Grid, Typography} from "@mui/material";
+import BuyPanel from "@src/components/BuyPanel";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import TabPanel from "@src/components/TabPanel";
+
+
+
 
 const Project: React.FC = () => {
+    const [activeTab, setActiveTab] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setActiveTab(newValue);
+    };
+
     return (
         <Layout>
-            <h1>Project</h1>
+            <div style={{backgroundImage:"url(/assets/images/favicon.png)"}}>
+                <Grid container spacing={2}>
+                    <Grid item xs={6} alignContent={"center"}>
+                        <Typography variant="h4" gutterBottom>
+                            プロジェクトコピー
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6} >
+                        <div style={{margin: '20px'}}>
+                            <BuyPanel />
+                        </div>
+                    </Grid>
+                </Grid>
+            </div>
+            <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                <Tabs value={activeTab} onChange={handleChange} centered>
+                    <Tab label="Overview" />
+                    <Tab label="Reports" />
+                    <Tab label="Q&A" />
+                </Tabs>
+                <TabPanel value={activeTab} index={0}> Overview </TabPanel>
+                <TabPanel value={activeTab} index={1}> Reports </TabPanel>
+                <TabPanel value={activeTab} index={2}> Q&A </TabPanel>
+            </Box>
         </Layout>
     );
 }
